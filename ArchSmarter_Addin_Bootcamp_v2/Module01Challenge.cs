b0793f6e9df2a04892dcf0c4f48ffa72d1115d7d
ABeckenham <84845336+ABeckenham@users.noreply.github.com>
2023-08-11 11:11:53 +0100

    Add project files.

---
+++ ArchSmarter_Addin_Bootcamp_v2/Module01Challenge.cs
@@ -0,0 +1,130 @@
+#region Namespaces
+using ArchSmarter_Addin_Bootcamp_v2;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Controls;
+
+#endregion
+
+namespace ArchSmarter_Addin_Bootcamp_v2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Module01Challenge : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // this is a variable for the Revit application
+            UIApplication uiapp = commandData.Application;
+
+            // this is a variable for the current Revit model
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+
+            //declare a number variable and set it to 250
+            int num1 = 250;
+            //declare a starting elevation variable and set it to 0
+            double ele1 = 0;
+            //declare a floor height variable and set it to 15
+            double floorHeight1 = 15;
+
+            //create filters to collect the floor, ceiling plan views
+            FilteredElementCollector ViewCollector = new FilteredElementCollector(doc);
+            ViewCollector.OfClass(typeof(ViewFamilyType));
+
+            ViewFamilyType floorPlanVFT = null;
+            foreach (ViewFamilyType curVFT in ViewCollector)
+            {
+                if (curVFT.ViewFamily == ViewFamily.FloorPlan)
+                {
+                    floorPlanVFT = curVFT;
+                }
+            }
+
+            ViewFamilyType ceilingPlanVFT = null;
+            foreach (ViewFamilyType curVFT in ViewCollector)
+            {
+                if (curVFT.ViewFamily == ViewFamily.CeilingPlan)
+                {
+                    ceilingPlanVFT = curVFT;
+                }
+            }
+
+            //create filter to collect the right titleblock to create the sheet
+            FilteredElementCollector sheetCollector = new FilteredElementCollector(doc);
+            sheetCollector.OfCategory(BuiltInCategory.OST_TitleBlocks);
+
+            //create a transaction to lock the model
+            Transaction t = new Transaction(doc);
+            t.Start("Create 250 floor levels");
+
+
+            //for every number in 230, create a floor plan         
+
+            for (int i = 0; i <= num1; i++)
+            {
+                ele1 = i * floorHeight1;
+                Level newLevel = Level.Create(doc, ele1); //this is not working                          
+                newLevel.Name = "New Level_" + i.ToString();
+
+
+
+                if (i % 3 == 0 && i % 5 == 0) //this is not working
+                {
+                    //create a sheet 
+                    ViewSheet newSheet = ViewSheet.Create(doc, sheetCollector.FirstElement().Id);
+                    newSheet.Name = "FIZZBUZZ_" + i;
+                    //create floorplan 
+                    ViewPlan floorPlan = ViewPlan.Create(doc, floorPlanVFT.Id, newLevel.Id);
+                    floorPlan.Name = "FIZZBUZZ_" + i;
+                    XYZ p = new XYZ(1, 1, 1);
+                    Viewport.Create(doc, newSheet.Id, floorPlan.Id, p);
+
+                }
+                else if (i % 3 == 0) //not by 5 as well
+                {
+                    //create floorplan 
+                    ViewPlan floorPlan = ViewPlan.Create(doc, floorPlanVFT.Id, newLevel.Id);
+                    floorPlan.Name = "FIZZ_" + i;
+                }
+                else if (i % 5 == 0) //not by 3 as well
+                {
+                    //create a ceilingplan
+                    ViewPlan ceilingPlan = ViewPlan.Create(doc, ceilingPlanVFT.Id, newLevel.Id);
+                    ceilingPlan.Name = "BUZZ_" + i;
+                }
+
+            }
+
+            //make a change in the revit model
+            t.Commit();
+            t.Dispose();
+
+
+
+            return Result.Succeeded;
+        }
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnCommand1";
+            string buttonTitle = "Button 1";
+
+            ButtonDataClass myButtonData1 = new ButtonDataClass(
+                buttonInternalName,
+                buttonTitle,
+                MethodBase.GetCurrentMethod().DeclaringType?.FullName,
+                Properties.Resources.Blue_32,
+                Properties.Resources.Blue_16,
+                "This is a tooltip for Button 1");
+
+            return myButtonData1.Data;
+        }
+    }
+}
---
+++ ArchSmarter_Addin_Bootcamp_v2/Module02Challenge.cs
@@ -0,0 +1,215 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Media.Animation;
+
+#endregion
+
+namespace ArchSmarter_Addin_Bootcamp_v2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Module02Challenge : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // this is a variable for the Revit application
+            UIApplication uiapp = commandData.Application;
+
+            // this is a variable for the current Revit model
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            // this is a variable to collect the current active document
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            IList<Element> pickList = uidoc.Selection.PickElementsByRectangle("Select Elements");
+
+            //filter more specificially 
+            List<CurveElement> modelCurves = new List<CurveElement>();
+            foreach (Element elem in pickList)
+            {
+                if (elem is CurveElement)
+                {
+                    // cast Element type to a CurveElement type 
+                    // they already are Curve elements because of the If Statement
+                    // casting them to their more specific CurveElement Type gives us
+                    // access different parameters
+                    // CurveElement curveElem = (CurveElementr)elem;
+
+                    CurveElement curveElem = elem as CurveElement;
+                    if (curveElem.CurveElementType == CurveElementType.ModelCurve)
+                    {
+                        Curve currentCurve = curveElem.GeometryCurve;
+                        modelCurves.Add(curveElem);
+                    }
+                }
+            }
+
+            TaskDialog.Show("Selected Elements", "  Selected in total: " + pickList.Count.ToString()
+               + "Selected ModelLines: " + modelCurves.Count.ToString());
+            //TaskDialog.Show("Selected Elements", $"You have selected " {modelCurves.Count} "ModelLines");
+            //why is this not working?
+
+            //Get type using methods
+            Level theLevel = GetLevelbyName(doc, "Level !");
+
+            Element projDuct = GetDuctTypeByName(doc,"Default");
+            Element projPipe = GetPipeTypeByName(doc, "Default");                       
+
+            WallType genWall = GetWallTypeByName(doc, @"Generic - 8""");
+            WallType storeWall = GetWallTypeByName(doc, "Storefront");
+            MEPSystemType ductSystemType = GetSystemTypeByName(doc, "Supply Air");
+            MEPSystemType pipeSystemType = GetSystemTypeByName(doc, "Domestic Hot Water");
+
+            //create transaction with using statement
+
+            using (Transaction t = new Transaction(doc))
+            {
+                t.Start("create revit elements");
+
+                foreach (CurveElement curCurve in modelCurves)
+                {
+                    Curve curve = curCurve.GeometryCurve;
+                    GraphicsStyle curStyle = curCurve.LineStyle as GraphicsStyle;
+
+                    //skip the circles that dont have endpoints
+                    if (curve.IsBound == false)
+                        continue;
+
+                    XYZ startPoint = curve.GetEndPoint(0);
+                    XYZ endPoint = curve.GetEndPoint(1);
+
+                    switch (curStyle.Name)
+                    {
+                        case "A-GLAZ":
+                            Wall.Create(doc, curve, storeWall.Id, theLevel.Id, 20, 0, false, false);
+                            break;
+
+                        case "A-WALL":
+                            Wall.Create(doc, curve, genWall.Id, theLevel.Id, 20, 0, false, false);
+                            break;
+
+                        case "M-DUCT":                            
+                            Duct.Create(doc, ductSystemType.Id, projDuct.Id, theLevel.Id, startPoint, endPoint);
+                            break;
+
+                        case "P-PIPE":                            
+                            Pipe.Create(doc, pipeSystemType.Id, projPipe.Id, theLevel.Id, startPoint, endPoint);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                t.Commit();
+
+            }
+            //BONUS - create a custom method to "get wall type by name", "get system type by name", DONE
+            //"create wall", "create pipe", "crate duct"
+
+            return Result.Succeeded;
+        }
+
+
+        //my methods below
+        
+        private static Level GetLevelbyName(Document doc, string LevelName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(Level));
+            collector.WhereElementIsElementType();
+            foreach (Level level in collector)
+            {
+                if (level.Name == LevelName)
+                    return level;
+            }
+            return null;
+        }
+
+        
+        internal WallType GetWallTypeByName(Document doc, String typeName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(WallType));
+
+            foreach (WallType curType in collector)
+            {
+                if (curType.Name == typeName)
+                {
+                    return curType;
+                }
+            }
+            return null;
+        }
+
+
+        internal MEPSystemType GetSystemTypeByName(Document doc, string typeName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(MEPSystemType));
+
+            foreach (MEPSystemType curType in collector)
+            {
+                if (curType.Name == typeName)
+                {
+                    return curType;
+                }
+            }
+            return null;
+        }
+
+        internal MEPSystemType GetDuctTypeByName(Document doc, string typeName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(DuctType));
+
+            foreach (MEPSystemType curType in collector)
+            {
+                if (curType.Name == typeName)
+                {
+                    return curType;
+                }
+            }
+            return null;
+        }
+
+        internal MEPSystemType GetPipeTypeByName(Document doc, string typeName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(PipeType));
+
+            foreach (MEPSystemType curType in collector)
+            {
+                if (curType.Name == typeName)
+                {
+                    return curType;
+                }
+            }
+            return null;
+        }
+
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnCommand1";
+            string buttonTitle = "Button 1";
+
+            ButtonDataClass myButtonData1 = new ButtonDataClass(
+                buttonInternalName,
+                buttonTitle,
+                MethodBase.GetCurrentMethod().DeclaringType?.FullName,
+                Properties.Resources.Blue_32,
+                Properties.Resources.Blue_16,
+                "This is a tooltip for Button 1");
+
+            return myButtonData1.Data;
+        }
+    }
+}
